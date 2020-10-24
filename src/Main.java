5c3d48fc4e0981b7481fa2278f7804eace2ddc25
ArmorDs <zeleznoff84-00@mail.ru>
2020-10-24 19:26:03 +0300

    mini upgrade

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.*;
 public class Main{
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
-            int n;
-            while(n<)
+            int n=1;
+            while(n<5){
+                System.out.println(1);
+                n++;
+            }
+
         }
 }
