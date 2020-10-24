29863efed36701c86886b8215ad3237ddf551f26
ArmorDs <zeleznoff84-00@mail.ru>
2020-10-24 19:02:13 +0300

    Upgrade and add .gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.util.*;
 public class Main{
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
-            int a, b;
-            a = in.nextInt();
-            b = in.nextInt();
-            int c = a + b;
-            System.out.println(c);
+            int n;
+            while(n<)
         }
 }
